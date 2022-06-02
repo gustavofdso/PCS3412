7c679df38405ee8d8626132f625ac0fb5a942995
Gustavo Oliveira <gustavodesa.10@gmail.com>
2022-06-01 21:15:20 -0300

    changed worktree

---
+++ FD.vhd
@@ -0,0 +1,15 @@
+-- PCS3412 - Organizacao e Arquitetura de Computadores I
+-- Project: T-FIVE-MC
+-- File: FD.vhd
+-- Author: Gustavo Freitas de Sá Oliveira
+--
+-- Description:
+--     Contém o fluxo de dados, com todos os seus componentes ligados
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+library work;
+use work.constants.all;
+use work.types.all;

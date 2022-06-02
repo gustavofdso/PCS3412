701ca51e0b5164241a58de4ef7f9cfa519391e17
Gustavo Oliveira <gustavodesa.10@gmail.com>
2022-06-01 21:46:15 -0300

    started FD

--- FD.vhd
+++ FD.vhd
@@ -7,7 +7,7 @@
 --
 -------------------------------------------------------------------------------
 --
--- Description : Contém a entidade do Fluxo de Dados e seus componentes.
+-- Description : Contains the details of the Data Flow entity and its components.
 --
 -------------------------------------------------------------------------------
 
@@ -21,20 +21,36 @@ use work.types.all;
 
 entity FD is
     port (
-        -- Clock and Reset input signal
-        clk:    in std_logic;
-        rst:    in std_logic;
+        -- Global Clock and Reset signals
+        clk:        in std_logic;
+        rst:        in std_logic;
 
         -- Clock enable for PC and IR
-        ce_pc:  in std_logic;
-        ce_ri:  in std_logic;
+        ce_pc:      in std_logic;
+        ce_ri:      in std_logic;
 
         -- Branch Control signal
-        Brch:   in std_logic_vector(1 downto 0);
+        Brch:       in std_logic_vector(1 downto 0);
+
+        -- Data Memory Control signals
+        menable:    in std_logic;
+        rw:         in std_logic;
+
+        -- Register Bank Control signals
+        MemtoReg:   in std_logic;
+        RegWrite:   in std_logic;
+        RegDest:    in std_logic;
 
         -- ALU Control signals
-        ALUSrc: in std_logic;
-        ALUOpe: in std_logic_vector(3 downto 0);
+        ALUSrc:     in std_logic;
+        ALUOpe:     in std_logic_vector(3 downto 0);
+
+        -- Operation code
+        Cop:        out std_logic_vector(5 downto 0);
+        CopExt:     out std_logic_vector(5 downto 0);
+
+        -- Zero indication for ALU
+        Zero:       out std_logic;
     ) ;
 end FD;
 
